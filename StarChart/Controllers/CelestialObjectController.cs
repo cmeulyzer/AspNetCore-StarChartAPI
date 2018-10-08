03290bacba301d9e7618d74cc0b0cb202d4c7bd3
cmeulyzer <christophmeulyzer@hotmail.com>
2018-10-09 00:52:15 +0200

    corr. [HttpDelete("{id}")]

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -80,7 +80,7 @@ namespace StarChart.Controllers
             return NoContent();
         }
 
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
             var celestialObjects = _context.CelestialObjects.Where(e => e.Id == id || e.OrbitedObjectId == id);
